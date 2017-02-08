1aacb21579f113be1289d04803226d09c3d34294
Aly Khayrulina <a.a.khayrulina@gmail.com>
2017-02-08 10:04:08 -0500

    imported components

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute } from 'react-router';
 import AppContainer from './containers/AppContainer';
+import Album from './containers/Album';
+import Albums from './containers/Albums';
+import Player from './containers/Player';
+import Sidebar from './containers/Sidebar';
+import Songs from './containers/Songs';
 
 ReactDOM.render(
   <AppContainer />,
